4287783a267dcc2d2a348564dfd1dbbc0cf6222b
faiyuching <faiyuching@magic.local>
2020-12-16 04:25:13 +0800

    Added show user

--- user/src/app.ts
+++ user/src/app.ts
@@ -7,6 +7,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { showUserRouter } from './routes/show';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
@@ -24,6 +25,7 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+app.use(showUserRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
