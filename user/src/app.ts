a8345f1e6ddee97c7ddfcebd9fe2737a6c990aff
faiyuching <faiyuching@magic.local>
2020-12-17 16:27:30 +0800

    add cors

--- user/src/app.ts
+++ user/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
+var cors = require('cors')
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -12,6 +13,7 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.use(cors())
 app.set('trust proxy', true);
 app.use(json());
 app.use(
