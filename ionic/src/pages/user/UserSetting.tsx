0ac3d9b94f0185fcf2938934862e11cc6cabca4a
faiyuching <faiyuching@magic.local>
2020-12-23 19:57:56 +0800

    update user setting page

--- ionic/src/pages/user/UserSetting.tsx
+++ ionic/src/pages/user/UserSetting.tsx
@@ -3,7 +3,7 @@ import {
   IonContent, IonButtons, IonMenuButton, IonTitle,
   IonCard, IonList, IonItem, IonAvatar,
   IonLabel, IonCardTitle, IonCardSubtitle,
-  IonGrid, IonRow, IonCol, IonPopover, IonInput, IonButton
+  IonPopover, IonInput, IonButton
 } from '@ionic/react';
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
@@ -87,79 +87,31 @@ const UserSetting: React.FC = () => {
                 </>
               </IonPopover>
               <IonItem button color="light" lines="none" onClick={() => setShowPopover({ showPopover: true, event: undefined, key: "email", value: `${userInfo.email}`, name: "邮箱" })}>
-                <IonGrid>
-                  <IonRow>
-                    <IonCol size-lg="4" size-md="3" size-sm="2">
-                      <IonCardSubtitle>邮箱</IonCardSubtitle>
-                    </IonCol>
-                    <IonCol size-lg="8" size-md="9" size-sm="10">
-                      <IonLabel className="ion-text-wrap">{userInfo.email}</IonLabel>
-                    </IonCol>
-                  </IonRow>
-                </IonGrid>
+                <IonCardSubtitle slot="start">邮箱</IonCardSubtitle>
+                <IonLabel slot="end">{userInfo.email}</IonLabel>
               </IonItem>
               <IonItem button lines="none" onClick={() => setShowPopover({ showPopover: true, event: undefined, key: "username", value: `${userInfo.username}`, name: "用户名" })}>
-                <IonGrid>
-                  <IonRow>
-                    <IonCol size-lg="4" size-md="3" size-sm="2">
-                      <IonCardSubtitle>用户名</IonCardSubtitle>
-                    </IonCol>
-                    <IonCol size-lg="8" size-md="9" size-sm="10">
-                      <IonLabel className="ion-text-wrap">{userInfo.username}</IonLabel>
-                    </IonCol>
-                  </IonRow>
-                </IonGrid>
+                <IonCardSubtitle slot="start">用户名</IonCardSubtitle>
+                <IonLabel slot="end">{userInfo.username}</IonLabel>
               </IonItem>
               <IonItem button color="light" lines="none" onClick={() => setShowPopover({ showPopover: true, event: undefined, key: "password", value: `${userInfo.username}`, name: "密码" })}>
-                <IonGrid>
-                  <IonRow>
-                    <IonCol size-lg="4" size-md="3" size-sm="2">
-                      <IonCardSubtitle>密码</IonCardSubtitle>
-                    </IonCol>
-                    <IonCol size-lg="8" size-md="9" size-sm="10">
-                      <IonLabel className="ion-text-wrap">........</IonLabel>
-                    </IonCol>
-                  </IonRow>
-                </IonGrid>
+                <IonCardSubtitle slot="start">密码</IonCardSubtitle>
+                <IonLabel slot="end">........</IonLabel>
               </IonItem>
               <IonItem button lines="none" onClick={() => setShowPopover({ showPopover: true, event: undefined, key: "skill", value: `${userInfo.skill}`, name: "专业/技能" })}>
-                <IonGrid>
-                  <IonRow>
-                    <IonCol size-lg="4" size-md="3" size-sm="2">
-                      <IonCardSubtitle>专业/技能</IonCardSubtitle>
-                    </IonCol>
-                    <IonCol size-lg="8" size-md="9" size-sm="10">
-                      <IonLabel className="ion-text-wrap">{userInfo.skill}</IonLabel>
-                    </IonCol>
-                  </IonRow>
-                </IonGrid>
+                <IonCardSubtitle slot="start">专业/技能</IonCardSubtitle>
+                <IonLabel slot="end">{userInfo.skill}</IonLabel>
               </IonItem>
               <IonItem button color="light" lines="none" onClick={() => setShowPopover({ showPopover: true, event: undefined, key: "intro", value: `${userInfo.intro}`, name: "介绍" })}>
-                <IonGrid>
-                  <IonRow>
-                    <IonCol size-lg="4" size-md="3" size-sm="2">
-                      <IonCardSubtitle>介绍</IonCardSubtitle>
-                    </IonCol>
-                    <IonCol size-lg="8" size-md="9" size-sm="10">
-                      <IonLabel className="ion-text-wrap">{userInfo.intro}</IonLabel>
-                    </IonCol>
-                  </IonRow>
-                </IonGrid>
+                <IonCardSubtitle slot="start">介绍</IonCardSubtitle>
+                <IonLabel slot="end">{userInfo.intro}</IonLabel>
               </IonItem>
               <IonItem lines="none">
-                <IonGrid>
-                  <IonRow>
-                    <IonCol size-lg="4" size-md="3" size-sm="2">
-                      <IonCardSubtitle>角色</IonCardSubtitle>
-                    </IonCol>
-                    <IonCol size-lg="8" size-md="9" size-sm="10">
-                      <IonLabel className="ion-text-wrap">
-                        {userInfo.role === "volunteer" && "志愿者"}
-                        {userInfo.role === "admin" && "管理员"}
-                      </IonLabel>
-                    </IonCol>
-                  </IonRow>
-                </IonGrid>
+                <IonCardSubtitle slot="start">角色</IonCardSubtitle>
+                <IonLabel slot="end">
+                  {userInfo.role === "volunteer" && "志愿者"}
+                  {userInfo.role === "admin" && "管理员"}
+                </IonLabel>
               </IonItem>
             </IonList>
           </IonCard>
