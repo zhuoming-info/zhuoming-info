0739901998e8338c8ef50cb3dc66dd0b48bedf21
faiyuching <faiyuching@magic.local>
2020-12-25 02:50:13 +0800

    update post service

--- ionic/src/pages/community/NewPost.tsx
+++ ionic/src/pages/community/NewPost.tsx
@@ -5,6 +5,7 @@ import {
   IonButtons, IonButton,
   IonLabel, IonBackButton, IonTextarea
 } from '@ionic/react';
+import Signin from '../Signin'
 import axios from 'axios';
 
 const NewPost: React.FC = () => {
@@ -19,27 +20,34 @@ const NewPost: React.FC = () => {
         console.log(error);
       })
   };
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonBackButton defaultHref="/community" text="取消"/>
-          </IonButtons>
-          <IonTitle>发帖</IonTitle>
-          <IonButtons slot="end">
-            <IonButton onClick={onSubmit}>发送</IonButton>
-          </IonButtons>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonItem lines="none">
-          <IonLabel position="floating">请输入：</IonLabel>
-          <IonTextarea rows={25} value={content} onIonChange={e => setContent(e.detail.value!)}></IonTextarea>
-        </IonItem>
-      </IonContent>
-    </IonPage>
-  );
+  if (!localStorage.getItem("userId")) {
+    return (
+      <Signin />
+    )
+  } else {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonBackButton defaultHref="/community" text="取消" />
+            </IonButtons>
+            <IonTitle>发帖</IonTitle>
+            <IonButtons slot="end">
+              <IonButton onClick={onSubmit}>发送</IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+          <IonItem lines="none">
+            <IonLabel position="floating">请输入：</IonLabel>
+            <IonTextarea rows={25} value={content} onIonChange={e => setContent(e.detail.value!)}></IonTextarea>
+          </IonItem>
+        </IonContent>
+      </IonPage>
+    );
+  }
+
 };
 
 export default NewPost;
