325ab3addffe610ee82d17bf3f625798d9ee3de8
faiyuching <faiyuching@magic.local>
2021-01-03 00:32:14 +0800

    update

--- ionic/src/components/HeaderLarge.tsx
+++ ionic/src/components/HeaderLarge.tsx
@@ -12,16 +12,18 @@ const HeaderLarge: React.FC<ContainerProps> = ({ title }) => {
     <IonHeader>
       <IonToolbar>
         <IonButtons slot="start">
-          <IonTitle>卓明</IonTitle>
+          <IonButton>
+            <IonTitle>卓明</IonTitle>
+          </IonButton>
           <IonButton color={title === "monitor" ? "primary" : "dark"} routerLink={'/monitor'}>监测</IonButton>
           <IonButton color={title === "response" ? "primary" : "dark"} routerLink={'/response'}>响应</IonButton>
           <IonButton color={title === "product" ? "primary" : "dark"} routerLink={'/product'}>产品</IonButton>
           <IonButton color={title === "forum" ? "primary" : "dark"} routerLink={'/forum'}>论坛</IonButton>
-          <IonButton color="dark" target="_blank" href="https://zhuoming-info.github.io/">
-            <IonIcon slot="start" icon={logoGithub} />参与开发
-            </IonButton>
         </IonButtons>
         <IonButtons slot="end">
+          <IonButton color="dark" target="_blank" href="https://zhuoming-info.github.io/">
+            <IonIcon slot="start" icon={logoGithub} />参与开发
+          </IonButton>
           {localStorage.getItem("userId") ? (
             <IonButton color="primary" routerLink={'/user'}>
               <IonIcon icon={personOutline} size="small" slot="start" />个人中心
