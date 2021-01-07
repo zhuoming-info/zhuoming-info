b5c37fb80f9c1fdaec193ff56815771e6d9e8d3c
faiyuching <faiyuching@magic.local>
2021-01-08 07:55:41 +0800

    update

--- ionic/src/components/monitor/JiangxiRiverWarningCover.tsx
+++ ionic/src/components/monitor/JiangxiRiverWarningCover.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import {
   IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle,
-  IonCardContent, IonList, IonListHeader
+  IonCardContent,
+  IonItem
 } from '@ionic/react';
 
 const JiangxiRiverWarningCover: React.FC = () => {
@@ -12,19 +13,10 @@ const JiangxiRiverWarningCover: React.FC = () => {
         <IonCardSubtitle>更新时间: 2020-12-27 16:50:15</IonCardSubtitle>
       </IonCardHeader>
       <IonCardContent>
-        {/* <h1 class='title'>暂无最近24小时内，1小时雨量超过30毫米的测站</h1>\n<h1 class='title'>暂无最近24小时内，3小时雨量超过50毫米的测站</h1>\n<h1 class='title'>暂无24小时内雨量超过80毫米的测站</h1>\n<br><center>更新时间: 2020-12-27 16:50:15</center> */}
-        <IonList>
-          <IonListHeader>暂无最近24小时内，1小时雨量超过30毫米的测站</IonListHeader>
-          {/* <IonItem>暂无最近24小时内，1小时雨量超过30毫米的测站</IonItem> */}
-          <IonListHeader>暂无最近24小时内，3小时雨量超过50毫米的测站</IonListHeader>
-          {/* <IonItem>暂无最近24小时内，3小时雨量超过50毫米的测站</IonItem> */}
-          <IonListHeader>暂无24小时内雨量超过80毫米的测站</IonListHeader>
-          {/* <IonItem>暂无24小时内雨量超过80毫米的测站</IonItem> */}
-        </IonList>
+        <IonItem lines='none'>暂无最近24小时内，1小时雨量超过30毫米的测站</IonItem>
+        <IonItem lines='none'>暂无最近24小时内，3小时雨量超过50毫米的测站</IonItem>
+        <IonItem lines='none'>最近24小时内雨量超过80毫米的测站1条</IonItem>
       </IonCardContent>
-      <IonCardHeader>
-        <IonCardSubtitle>数据来源:http://slt.jiangxi.gov.cn/col/col29334/index.html</IonCardSubtitle>
-      </IonCardHeader>
     </IonCard>
   );
 };
