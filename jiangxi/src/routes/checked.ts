a0ae02864183d27df6cb896ee107c00e96785da5
faiyuching <faiyuching@magic.local>
2021-01-09 01:32:32 +0800

    update

---
+++ jiangxi/src/routes/checked.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from 'express';
+import axios from 'axios';
+
+const router = express.Router();
+
+router.get('/api/jiangxi/river-warning', async (req: Request, res: Response) => {
+  const checked = req.query.checked;
+
+  const getHtml = () => {
+    axios.get('http://111.75.205.67:7080/syq/findsliderain/findSlideRainHandler')
+      .then((res) => {
+        if (res.data.msg === '执行成功' && res.data.status === 1) {
+          console.log(res.data.data)
+        } else {
+          console.log(res.data)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  let nIntervId;
+  if (checked === 'true') {
+    nIntervId = setInterval(getHtml, 5000);
+  } else if (checked === 'false') {
+    clearInterval(nIntervId)
+  }
+
+  res.send();
+});
+
+export { router as checkedRouter };
