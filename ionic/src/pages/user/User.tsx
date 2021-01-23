bd41c610a1482d6b37a34d3eb607c017238aa3ef
faiyuching <faiyuching@magic.local>
2021-01-23 12:47:13 +0800

    update

--- ionic/src/pages/user/User.tsx
+++ ionic/src/pages/user/User.tsx
@@ -32,28 +32,24 @@ const User: React.FC = () => {
               <IonItem>
                 <IonLabel>
                   <h2>Poe</h2>
-                  <h3>New Ride</h3>
                   <p>I just upgraded my X-Wing. Next time...</p>
                 </IonLabel>
               </IonItem>
               <IonItem>
                 <IonLabel>
                   <h2>Ben</h2>
-                  <h3>Move Along</h3>
                   <p>These aren't the droids you're looking for...</p>
                 </IonLabel>
               </IonItem>
               <IonItem>
                 <IonLabel>
                   <h2>Leia</h2>
-                  <h3>You're My Only Hope</h3>
                   <p>I've placed information vital to the survival...</p>
                 </IonLabel>
               </IonItem>
               <IonItem>
                 <IonLabel>
                   <h2>Yoda</h2>
-                  <h3>Size matters not</h3>
                   <p>Do or do not. There is no try...</p>
                 </IonLabel>
               </IonItem>
@@ -61,28 +57,24 @@ const User: React.FC = () => {
               <IonItem>
                 <IonLabel>
                   <h2>Finn</h2>
-                  <h3>I'm a big deal</h3>
                   <p>Listen, I've had a pretty messed up day...</p>
                 </IonLabel>
               </IonItem>
               <IonItem>
                 <IonLabel>
                   <h2>Han</h2>
-                  <h3>Look, kid...</h3>
                   <p>I've got enough on my plate as it is, and I...</p>
                 </IonLabel>
               </IonItem>
               <IonItem>
                 <IonLabel>
                   <h2>Rey</h2>
-                  <h3>I can handle myself</h3>
                   <p>You will remove these restraints and leave...</p>
                 </IonLabel>
               </IonItem>
               <IonItem>
                 <IonLabel>
                   <h2>Luke</h2>
-                  <h3>Your thoughts betray you</h3>
                   <p>I feel the good in you, the conflict...</p>
                 </IonLabel>
               </IonItem>
