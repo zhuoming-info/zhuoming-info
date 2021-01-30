b27b0b745db7235218762ff8cd24e398af7c81d7
faiyuching <faiyuching@magic.local>
2021-01-30 23:09:00 +0800

    update

--- ionic/src/components/response/response/Member.tsx
+++ ionic/src/components/response/response/Member.tsx
@@ -1,60 +1,53 @@
 import {
-  IonItem, IonList, IonAvatar, IonLabel, IonImg, IonListHeader,
-  IonGrid, IonRow, IonCol, IonIcon
+  IonItem, IonList, IonAvatar, IonLabel, IonImg, IonListHeader, IonIcon
 } from '@ionic/react';
 import React from 'react';
 import { add } from 'ionicons/icons';
 
 const Member: React.FC = () => {
   return (
-    <IonGrid>
-      <IonRow>
-        <IonCol>
-          <IonList>
-            <IonListHeader>组长</IonListHeader>
-            <IonItem button routerLink={'/user'}>
-              <IonAvatar slot="start">
-                <IonImg src="https://res.wx.qq.com/a/wx_fed/webwx/res/static/img/2KriyDK.png" />
-              </IonAvatar>
-              <IonLabel>faiyuching</IonLabel>
-            </IonItem>
-            <IonItem button routerLink={'/user'}>
-              <IonAvatar slot="start">
-                <IonImg src="https://res.wx.qq.com/a/wx_fed/webwx/res/static/img/2KriyDK.png" />
-              </IonAvatar>
-              <IonLabel>faiyuching</IonLabel>
-            </IonItem>
-            <IonItem button detailIcon="">
-              <IonIcon icon={add} size="small"/>
-              <IonLabel>添加成员</IonLabel>
-            </IonItem>
-            <IonListHeader>组员</IonListHeader>
-            <IonItem button routerLink={'/user'}>
-              <IonAvatar slot="start">
-                <IonImg src="https://res.wx.qq.com/a/wx_fed/webwx/res/static/img/2KriyDK.png" />
-              </IonAvatar>
-              <IonLabel>faiyuching</IonLabel>
-            </IonItem>
-            <IonItem button routerLink={'/user'}>
-              <IonAvatar slot="start">
-                <IonImg src="https://res.wx.qq.com/a/wx_fed/webwx/res/static/img/2KriyDK.png" />
-              </IonAvatar>
-              <IonLabel>faiyuching</IonLabel>
-            </IonItem>
-            <IonItem button routerLink={'/user'}>
-              <IonAvatar slot="start">
-                <IonImg src="https://res.wx.qq.com/a/wx_fed/webwx/res/static/img/2KriyDK.png" />
-              </IonAvatar>
-              <IonLabel>faiyuching</IonLabel>
-            </IonItem>
-            <IonItem button detailIcon="">
-              <IonIcon icon={add} size="small"/>
-              <IonLabel>添加成员</IonLabel>
-            </IonItem>
-          </IonList>
-        </IonCol>
-      </IonRow>
-    </IonGrid>
+    <IonList>
+      <IonListHeader>组长</IonListHeader>
+      <IonItem button routerLink={'/user'}>
+        <IonAvatar slot="start">
+          <IonImg src="https://res.wx.qq.com/a/wx_fed/webwx/res/static/img/2KriyDK.png" />
+        </IonAvatar>
+        <IonLabel>faiyuching</IonLabel>
+      </IonItem>
+      <IonItem button routerLink={'/user'}>
+        <IonAvatar slot="start">
+          <IonImg src="https://res.wx.qq.com/a/wx_fed/webwx/res/static/img/2KriyDK.png" />
+        </IonAvatar>
+        <IonLabel>faiyuching</IonLabel>
+      </IonItem>
+      <IonItem button detailIcon="">
+        <IonIcon icon={add} size="small" />
+        <IonLabel>添加成员</IonLabel>
+      </IonItem>
+      <IonListHeader>组员</IonListHeader>
+      <IonItem button routerLink={'/user'}>
+        <IonAvatar slot="start">
+          <IonImg src="https://res.wx.qq.com/a/wx_fed/webwx/res/static/img/2KriyDK.png" />
+        </IonAvatar>
+        <IonLabel>faiyuching</IonLabel>
+      </IonItem>
+      <IonItem button routerLink={'/user'}>
+        <IonAvatar slot="start">
+          <IonImg src="https://res.wx.qq.com/a/wx_fed/webwx/res/static/img/2KriyDK.png" />
+        </IonAvatar>
+        <IonLabel>faiyuching</IonLabel>
+      </IonItem>
+      <IonItem button routerLink={'/user'}>
+        <IonAvatar slot="start">
+          <IonImg src="https://res.wx.qq.com/a/wx_fed/webwx/res/static/img/2KriyDK.png" />
+        </IonAvatar>
+        <IonLabel>faiyuching</IonLabel>
+      </IonItem>
+      <IonItem button detailIcon="">
+        <IonIcon icon={add} size="small" />
+        <IonLabel>添加成员</IonLabel>
+      </IonItem>
+    </IonList>
   );
 };
 
